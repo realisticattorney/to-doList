0d09a1fd94be3003223782275ad5bc0a1b5326d1
Luis Vinicius <luisvinicius0906@gmail.com>
2021-04-07 16:36:53 -0300

    Add function to load initial array

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
-import sideNavbar from './sidenav';
-
-sideNavbar();
 
 
-//proyotype of tasks
-function Task(name) {
-  this.title = title;
-  this.description = description;
-  this.dueDate = dueDate;
-  this.priority = priority;
+// function Task(name) {
+//   this.title = title;
+//   this.description = description;
+//   this.dueDate = dueDate;
+//   this.priority = priority;
+// }
+
+import { update } from "lodash";
+
+import projectCreation from './project';
+import displayProjects from './project'
+
+projectCreation();
+// let firstTask = new Task("create prototype", "a bunch of properties into a function constructor", Date.now(), 1 );
+
+
+window.addEventListener('load', () => {
+
+  if(JSON.parse(localStorage.getItem('projects')) === null) {
+    localStorage.setItem('projects', JSON.stringify(new Array));
+  }
+
+  displayProjects();
+})
+
+
+
+
+// Tasks creation
+
+const Tasks = (title, desc, date, priority) => {
+
+
+  return { title, desc, date, priority }
 }
-
-
-
-// Only change code above this line
-let firstTask = new Task("create prototype", "a bunch of properties into a function constructor", Date.now(), 1 );
-
