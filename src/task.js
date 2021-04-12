4a3e401ae891272620412dd47cb1f49b7e56534e
Luis Vinicius <luisvinicius0906@gmail.com>
2021-04-12 19:06:01 -0300

    Fix linters errors in task.js

--- src/task.js
+++ src/task.js
@@ -1,6 +1,7 @@
 import {
-  updatedTasks, updatedProjects, updateTasks, validateInput, displayAll
+  updatedTasks, updatedProjects, updateTasks, validateInput,
 } from './utils';
+import { displayAll } from './filter';
 
 const taskName = document.querySelector('#task-name');
 const taskDesc = document.querySelector('#task-desc');
@@ -9,9 +10,7 @@ const taskPriority = document.querySelector('#task-priority');
 const taskProject = document.querySelector('#task-project');
 const taskBtn = document.querySelector('#task-btn');
 
-const createTask = (name, desc, date, priority, project) => {
-    name, desc, date, priority, project
-};
+const createTask = (name, desc, date, priority, project) => { name, desc, date, priority, project };
 
 const addNewTask = (name, desc, date, priority, project) => {
   const item = createTask(name, desc, date, priority, project);
@@ -42,6 +41,22 @@ const openTaskModal = () => {
   close.addEventListener('click', () => {
     modal.style.display = 'none';
   });
+};
+
+const saveTask = (id) => {
+  const editingName = document.querySelector('#editingName');
+  const editingDesc = document.querySelector('#editingDesc');
+  const editingDate = document.querySelector('#editingDate');
+  const editingPriority = document.querySelector('#editingPriority');
+  const editingProject = document.querySelector('#editingProject');
+
+  const editedTask = createTask(
+    editingName.value, editingDesc.value, editingDate.value, editingPriority.value, editingProject.value
+  );
+  const tempTasks = updatedTasks();
+  tempTasks[id] = editedTask;
+  updateTasks(tempTasks);
+  displayAll();
 };
 
 const openEdit = (id) => {
@@ -129,22 +144,6 @@ const openEdit = (id) => {
     saveTask(id);
     modalWrapper.remove();
   });
-};
-
-const saveTask = (id) => {
-  const editingName = document.querySelector('#editingName');
-  const editingDesc = document.querySelector('#editingDesc');
-  const editingDate = document.querySelector('#editingDate');
-  const editingPriority = document.querySelector('#editingPriority');
-  const editingProject = document.querySelector('#editingProject');
-
-  const editedTask = createTask(
-    editingName.value, editingDesc.value, editingDate.value, editingPriority.value, editingProject.value
-  );
-  const tempTasks = updatedTasks();
-  tempTasks[id] = editedTask;
-  updateTasks(tempTasks);
-  displayAll();
 };
 
 const taskCreation = () => {
