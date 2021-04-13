1a2c1e3c207c36fe5c6fb9222f3e8d0f4a10d517
realisticattorney <g.axl.aquila@gmail.com>
2021-04-13 19:55:33 -0300

    Add scontentt to edit task's modal

--- src/task.js
+++ src/task.js
@@ -23,3 +23,90 @@ const addNewTask = (name, desc, date, priority, project) => {
   taskDate.value = '';
   taskPriority.value = '';
 };
+
+/* eslint-disable */
+const saveTask = (id) => {
+  const editingName = document.querySelector('#editingName');
+  const editingDesc = document.querySelector('#editingDesc');
+  const editingDate = document.querySelector('#editingDate');
+  const editingPriority = document.querySelector('#editingPriority');
+  const editingProject = document.querySelector('#editingProject');
+
+  const editedTask = createTask(
+    editingName.value, editingDesc.value,
+    editingDate.value, editingPriority.value, editingProject.value,
+  );
+  const tempTasks = updatedTasks();
+  tempTasks[id] = editedTask;
+  updateTasks(tempTasks);
+  displayArrOfTasks(updatedTasks());
+};
+
+const deleteTask = (id) => {
+  const tasksArr = updatedTasks();
+  tasksArr.splice(id, 1);
+  updateTasks(tasksArr);
+  displayArrOfTasks(updatedTasks());
+};
+/* eslint-enable */
+
+const openEdit = (id) => {
+  const tempTasks = updatedTasks();
+  const tempProjects = updatedProjects();
+
+  const contentDiv = document.querySelector('#content');
+
+  const modalWrapper = document.createElement('div');
+  modalWrapper.classList.add('modal');
+  modalWrapper.id = 'taskModal';
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modalContent');
+
+  const closeBtn = document.createElement('span');
+  closeBtn.classList.add('close');
+  closeBtn.innerText = 'X';
+
+  const taskNameInput = document.createElement('input');
+  taskNameInput.value = tempTasks[id].name;
+  taskNameInput.id = 'editingName';
+
+  const taskDescInput = document.createElement('input');
+  taskDescInput.value = tempTasks[id].desc;
+  taskDescInput.id = 'editingDesc';
+
+  const taskDateInput = document.createElement('input');
+  taskDateInput.type = 'date';
+  taskDateInput.value = tempTasks[id].date;
+  taskDateInput.id = 'editingDate';
+
+  const taskPriorityInput = document.createElement('select');
+  const optionOne = document.createElement('option');
+  optionOne.innerText = 'Normal';
+  const optionTwo = document.createElement('option');
+  optionTwo.innerText = 'Urgent';
+  const optionThree = document.createElement('option');
+  optionThree.innerText = 'Later';
+  taskPriorityInput.append(optionOne, optionTwo, optionThree);
+  taskPriorityInput.value = tempTasks[id].priority;
+  taskPriorityInput.id = 'editingPriority';
+
+  const taskProjectInput = document.createElement('select');
+  for (let i = 0; i < tempProjects.length; i += 1) {
+    const projectOption = document.createElement('option');
+    projectOption.innerText = tempProjects[i].name;
+    taskProjectInput.append(projectOption);
+  }
+  taskProjectInput.value = tempTasks[id].project;
+  taskProjectInput.id = 'editingProject';
+
+  modalContent.appendChild(taskProjectInput);
+  modalContent.appendChild(taskPriorityInput);
+  modalContent.appendChild(taskDateInput);
+  modalContent.appendChild(taskDescInput);
+  modalContent.appendChild(taskNameInput);
+
+  modalContent.appendChild(closeBtn);
+  modalWrapper.appendChild(modalContent);
+  contentDiv.appendChild(modalWrapper);
+}
