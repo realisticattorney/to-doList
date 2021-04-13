ed783bbe368b0b9ccde83b5693528e25d6228ad8
realisticattorney <g.axl.aquila@gmail.com>
2021-04-13 18:53:10 -0300

    Add display Today projects function

--- src/filter.js
+++ src/filter.js
@@ -23,3 +23,20 @@ const displayPast = () => {
   );
   displayArrOfTasks(filteredArr);
 };
+
+const pastAction = () => {
+  const pastBtn = document.querySelector('#pastBtn');
+  pastBtn.addEventListener('click', () => {
+    displayPast();
+  });
+};
+
+const displayToday = () => {
+  const tasksArr = updatedTasks();
+  const filteredArr = tasksArr.filter(
+    (task) => (task.date.slice(8) === date.getDate().toString()
+    && task.date.slice(5, 7) === (date.getMonth() + 1).toString())
+  );
+  displayArrOfTasks(filteredArr);
+};
+/* eslint-enable  */
