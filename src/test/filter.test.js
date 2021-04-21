2759d398df23afb78f778a175f059951824dce1e
realisticattorney <g.axl.aquila@gmail.com>
2021-04-21 20:22:37 -0300

    Add task.js file tests

--- src/test/filter.test.js
+++ src/test/filter.test.js
@@ -1,19 +1,2 @@
 const filter = require('../filter');
 
-describe("create project", () => {
-  it("Create a new project", () => {
-    const idNumber = Date.now().toString();
-    const create = createList("Firt project");
-    expect(create).toEqual({
-      name: "Firt project"
-    });
-  });
-
-  it("If no input throws undefined", () => {
-    const idNumber = Date.now().toString();
-    const create = createList();
-    expect(create).toEqual({
-      name: undefined,
-    });
-  });
-});
