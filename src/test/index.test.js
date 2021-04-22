70b42ad76fac7b05cd766e4e438b3d5763ccc2b4
realisticattorney <g.axl.aquila@gmail.com>
2021-04-21 21:39:36 -0300

    Add jsdom to dependencies. Fix linters

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,41 +1,39 @@
-import jsdom from "jsdom";
+import jsdom from 'jsdom';
 
 const { JSDOM } = jsdom;
 
-describe("MODULE RETRIEVE PROJECTS TEST", () => {
-  beforeEach(() =>
-    JSDOM.fromFile("./dist/index.html").then((dom) => {
-      document.body.innerHTML = dom.window.document.body.outerHTML;
-    })
-  );
+describe('MODULE RETRIEVE PROJECTS TEST', () => {
+  beforeEach(() => JSDOM.fromFile('./dist/index.html').then((dom) => {
+    document.body.innerHTML = dom.window.document.body.outerHTML;
+  }));
 
   it("Expects the project's projects container to not be null", () => {
-    const container = document.querySelector("content");
+    const container = document.querySelector('content');
     expect(container).toBeDefined();
   });
 
   it("Expects the project's section to not be null", () => {
-    const section = document.querySelector(".projectContainer");
+    const section = document.querySelector('.projectContainer');
     expect(section).toBeDefined();
   });
 
   it("Expects the project's modal to not be null", () => {
-    const modal = document.querySelector(".projectModal");
+    const modal = document.querySelector('.projectModal');
     expect(modal).toBeDefined();
   });
 
   it("Expects the project's modal to not be null", () => {
-    const modal = document.querySelector(".projectContent");
+    const modal = document.querySelector('.projectContent');
     expect(modal).toBeDefined();
   });
 
   it("Expects the project's modal to not be null", () => {
-    const modal = document.querySelector(".projectName");
+    const modal = document.querySelector('.projectName');
     expect(modal).toBeDefined();
   });
 
   it("Expects the project's task container to not be null", () => {
-    const container = document.getElementsByClassName("closeProject");
+    const container = document.getElementsByClassName('closeProject');
     expect(container).toBeDefined();
   });
 });
--- src/test/project.test.js
+++ src/test/project.test.js
@@ -1,14 +1,14 @@
 const project = require('../project');
 
-describe("create project", () => {
-  it("Create a new project", () => {
-    const create = project.createProject("Firt project");
+describe('create project', () => {
+  it('Create a new project', () => {
+    const create = project.createProject('Firt project');
     expect(create).toEqual({
-      name: "Firt project"
+      name: 'Firt project',
     });
   });
 
-  it("If no input throws undefined", () => {
+  it('If no input throws undefined', () => {
     const create = project.createProject();
     expect(create).toEqual({
       name: undefined,
